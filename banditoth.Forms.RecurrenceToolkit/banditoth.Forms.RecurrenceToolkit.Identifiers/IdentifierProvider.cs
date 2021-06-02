204279d55d592d27359024f41c683b2080338107
banditoth <andrastoth1126@gmail.com>
2021-06-02 15:21:52 +0200

    Added IdentifierProvider project

---
+++ banditoth.Forms.RecurrenceToolkit/banditoth.Forms.RecurrenceToolkit.Identifiers/IdentifierProvider.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using Xamarin.Essentials;
+
+namespace banditoth.Forms.RecurrenceToolkit.Identifiers
+{
+    public static class IdentifierProvider
+    {
+        private const string UniqueIdentifierPreferenceKey = "IDENTIFIERPROVIDER_UNIQUEID";
+
+        public static string UniqueId
+        {
+            get
+            {
+                string stored = Preferences.Get(UniqueIdentifierPreferenceKey, null);
+                if ((VersionTracking.IsFirstLaunchEver && string.IsNullOrWhiteSpace(stored)) || string.IsNullOrWhiteSpace(stored))
+                {
+                    stored = Guid.NewGuid().ToString();
+                    Preferences.Set(UniqueIdentifierPreferenceKey, stored);
+                }
+
+                return stored;
+            }
+        }
+    }
+}
